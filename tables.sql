33dd758c629db4dd573b1c9940cb24c4c7ebf4da
gobinath-p-web <gobinathp98@gmail.com>
2025-12-20 19:30:34 +0530

    table commited

---
+++ tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE students (
+    regno VARCHAR(20) PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    address TEXT,
+    blood_group VARCHAR(5),
+    phone_number VARCHAR(15),
+    email VARCHAR(100) UNIQUE,
+    gender VARCHAR(10),
+    dob DATE
+);
